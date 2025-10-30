56ba389ffd944b61908dd05f29052a0702a328b8
Andrei Belov <a.belov@f5.com>
2025-10-29 18:59:07 -0600

    Clone of the linkerd branch with Debian 11 as a base distro
    It has 5.10 kernel and does not have iptables-nft by default,
    which makes it perfect target for experiments mirroring older
    EKS setups where Amazon Linux 2 is the base distro.

--- scripts/common.sh
+++ scripts/common.sh
@@ -20,6 +20,9 @@ EOF
 
 sudo systemctl restart systemd-resolved
 
+sudo rm /etc/resolv.conf
+sudo ln -s /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
 # disable swap
 sudo swapoff -a
 
@@ -52,6 +55,8 @@ sudo sysctl --system
 sudo apt-get update -y
 apt-get install -y software-properties-common curl apt-transport-https ca-certificates
 
+sudo mkdir -p /etc/apt/keyrings
+
 curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" |
@@ -66,7 +71,6 @@ sudo systemctl start crio.service
 
 echo "CRI runtime installed susccessfully"
 
-sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
