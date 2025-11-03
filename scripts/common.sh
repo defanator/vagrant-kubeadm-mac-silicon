e4a6c96dea54d360cace3d6b4b6e2e02c7a2040e
Andrei Belov <a.belov@f5.com>
2025-11-03 10:37:25 -0700

    Ensure kubelet service is enabled

--- scripts/common.sh
+++ scripts/common.sh
@@ -61,6 +61,8 @@ EOF
 
 sudo yum install -y kubelet kubectl kubeadm jq
 
+sudo systemctl enable kubelet --now
+
 local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
 cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
