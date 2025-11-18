c059508aedfe53874726273f13968d3d1a772c6b
Andrei Belov <defanator@gmail.com>
2025-11-17 22:26:36 -0700

    Wait for node readiness before labeling
    This is intended to address possible race condition between starting
    kubelet and attempting to modify a newborn node.

--- scripts/node.sh
+++ scripts/node.sh
@@ -10,12 +10,14 @@ config_path="/vagrant/configs"
 
 sudo -i -u vagrant bash << EOF
 
+set -x
 whoami
 mkdir -p /home/vagrant/.kube
 sudo cp -i $config_path/config /home/vagrant/.kube/
 sudo chown 1000:1000 /home/vagrant/.kube/config
 NODENAME=$(hostname -s)
 
-kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker
+kubectl wait node/\${NODENAME} --for=condition=Ready
 
+kubectl label node \${NODENAME} node-role.kubernetes.io/worker=worker
 EOF
